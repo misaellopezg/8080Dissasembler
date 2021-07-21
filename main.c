9df8f83b262f17b1307089c830fbc4b779002eb5
Misael Lopez <misael.lopez@sekisuiaerospace.com>
2021-07-20 18:03:15 -0700

    Restarted code!
    Signed-off-by: Misael Lopez <misael.lopez@sekisuiaerospace.com>

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-//HelloWorld!
+//This code will take 8080 assembly language code and translate its instructions
+//to human-readable instructions
+//Created by: Misael Lopez
+//Date: 07/15/2021
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	return 0;
+}
