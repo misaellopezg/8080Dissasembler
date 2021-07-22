e248e0add7a14f703c2563488ace805007feb617
Misael Lopez <misael.lopez@sekisuiaerospace.com>
2021-07-21 17:04:07 -0700

    Managed to read a portion of the invaders.h code!
    Signed-off-by: Misael Lopez <misael.lopez@sekisuiaerospace.com>

--- main.c
+++ main.c
@@ -3,9 +3,26 @@
 //Created by: Misael Lopez
 //Date: 07/15/2021
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-	printf("Hello World!\n");
+	//Create file pointer
+	FILE *fptr;
+	unsigned char buffer[255];
+	fptr = fopen("C:\\Users\\misael.lopez\\Desktop\\Dissasembler\\invaders.h","rb");
+	if(fptr == NULL)
+	{
+		printf("Error! Could not open file.\n");
+		exit(1);
+	}
+	else
+	{
+		while( fgets(buffer,255,fptr) != NULL)
+		{
+			puts(buffer);
+		}
+		fclose(fptr);
+	}
+	
 	return 0;
 }
